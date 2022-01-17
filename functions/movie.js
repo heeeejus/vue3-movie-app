eaed7fe4a51513776526d0bcfe2ba8c547434f18
heeeejus <mary2946@naver.com>
2022-01-18 00:04:23 +0900

    Modify server port number

--- functions/movie.js
+++ functions/movie.js
@@ -1,10 +1,12 @@
 const axios = require('axios')
+const OMDB_API_KEY = process.env.OMDB_API_KEY //환경변수로 지정
+
 //api키를 서버에서 쓸수 있도록
 exports.handler = async function (event) {
   console.log(event)
   const payload = JSON.parse(event.body)
   const { title, type, year, page, id } = payload
-  const OMDB_API_KEY = '7035c60c'
+
   const url = id 
     ? `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
     : `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
